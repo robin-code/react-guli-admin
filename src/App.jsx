d8da1ebc5a4aac978f55864c0b59679a0c7d16ef
robin-li <robin-develop@outlook.com>
2024-08-13 07:44:27 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import './App.css'
-import Hello from "./components/Hello"
-import Welcome from "./components/Welcome"
-import StateComponent from "./components/StateComponent.jsx";
 
 function App() {
 
     return (
         <>
-            <Hello/>
-            <Welcome/>
-            <StateComponent/>
+            App ...
         </>
     )
 }
