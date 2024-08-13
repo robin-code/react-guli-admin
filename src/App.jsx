612f20af18514efc238204cc355572ca9e829c72
robin-li <robin-develop@outlook.com>
2024-08-13 08:01:02 +0800

    Footer组件

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./index.css"
 import Header from "./components/Header";
+import Footer from "./components/Footer/index.jsx";
 
 function App() {
 
@@ -24,15 +25,7 @@ function App() {
                             <button className="btn btn-danger" style={{display: "none"}}>删除</button>
                         </li>
                     </ul>
-                    <div className="todo-footer">
-                        <label>
-                            <input type="checkbox"/>
-                        </label>
-                        <span>
-          <span>已完成0</span> / 全部2
-        </span>
-                        <button className="btn btn-danger">清除已完成任务</button>
-                    </div>
+                    <Footer/>
                 </div>
             </div>
         </>
