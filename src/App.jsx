daf01314e2d1503d68dd4fb6e5e19f28184fbb8e
robin-li <robin-develop@outlook.com>
2024-08-15 10:09:36 +0800

    github user static

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
 import {Component} from 'react';
-import axios from 'axios'
-
+import Search from "./Component/Search";
+import List from "./Component/List";
+import './bootstrap.css'
 class App extends Component {
-
-    getStudentData = () => {
-        axios.get('http://localhost:8080/api2/search/users2').then(
-            response => {
-                console.log('成功了', response.data);
-            },
-            error => {
-                console.log('失败了', error);
-            }
-        )
-    }
-
-    getGithubUser = () => {
-        axios.get('http://localhost:8080/api/search/users?q=robin').then(
-            response => {
-                console.log('成功了', response.data);
-            },
-            error => {
-                console.log('失败了', error);
-            }
-        )
-    }
 
     render() {
         return (
-            <div>
-                <button onClick={this.getStudentData}>点我获取学生数据</button>
-                <button onClick={this.getGithubUser}>点我GithubUsers</button>
+            <div className="container">
+                <Search/>
+                <List/>
             </div>
         );
     }
