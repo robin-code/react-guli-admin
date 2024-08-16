119bd92e93456e086e0aa239fc9418ad13259073
robin-li <robin-develop@outlook.com>
2024-08-16 17:51:56 +0800

    navlink 选中高亮

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Link,Route} from 'react-router-dom'
+import {Link, NavLink, Route} from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 
@@ -21,8 +21,8 @@ export default class App extends Component {
 							<a className="list-group-item active" href="./home.html">Home</a> */}
 
 							{/* 在React中靠路由链接实现切换组件--编写路由链接 */}
-							<Link className="list-group-item" to="/about">About</Link>
-							<Link className="list-group-item" to="/home">Home</Link>
+							<NavLink className="list-group-item" to="/about">About</NavLink>
+							<NavLink className="list-group-item" to="/home">Home</NavLink>
 						</div>
 					</div>
 					<div className="col-xs-6">
