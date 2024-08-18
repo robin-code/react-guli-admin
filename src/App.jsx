e8968acb3e5afd446703f611720677e046277580
robin-li <robin-develop@outlook.com>
2024-08-18 15:32:35 +0800

    样式丢失

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,8 @@ export default class App extends Component {
 							<a className="list-group-item active" href="./home.html">Home</a> */}
 
                             {/* 在React中靠路由链接实现切换组件--编写路由链接 */}
-                            <NavLink className="list-group-item" to="/about">About</NavLink>
-                            <NavLink className="list-group-item" to="/home">Home</NavLink>
+                            <NavLink className="list-group-item" to="/robin/about">About</NavLink>
+                            <NavLink className="list-group-item" to="/robin/home">Home</NavLink>
                         </div>
                     </div>
                     <div className="col-xs-6">
@@ -30,8 +30,8 @@ export default class App extends Component {
                             <div className="panel-body">
                                 {/* 注册路由 */}
                                 <Switch>
-                                    <Route path="/about" component={About}/>
-                                    <Route path="/home" component={Home}/>
+                                    <Route path="/robin/about" component={About}/>
+                                    <Route path="/robin/home" component={Home}/>
                                 </Switch>
                             </div>
                         </div>
