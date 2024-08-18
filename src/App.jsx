457a3dde87102aed05c50a9a9970f5d9658c842e
robin-li <robin-develop@outlook.com>
2024-08-18 15:38:55 +0800

    redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {NavLink, Route, Switch} from 'react-router-dom'
+import {NavLink, Redirect, Route, Switch} from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 
@@ -21,8 +21,8 @@ export default class App extends Component {
 							<a className="list-group-item active" href="./home.html">Home</a> */}
 
                             {/* 在React中靠路由链接实现切换组件--编写路由链接 */}
-                            <NavLink className="list-group-item" to="/robin/about">About</NavLink>
-                            <NavLink className="list-group-item" to="/robin/home">Home</NavLink>
+                            <NavLink className="list-group-item" to="/about">About</NavLink>
+                            <NavLink className="list-group-item" to="/home">Home</NavLink>
                         </div>
                     </div>
                     <div className="col-xs-6">
@@ -30,8 +30,9 @@ export default class App extends Component {
                             <div className="panel-body">
                                 {/* 注册路由 */}
                                 <Switch>
-                                    <Route path="/robin/about" component={About}/>
-                                    <Route path="/robin/home" component={Home}/>
+                                    <Route path="/about" component={About}/>
+                                    <Route path="/home" component={Home}/>
+									<Redirect to='/home' />
                                 </Switch>
                             </div>
                         </div>
