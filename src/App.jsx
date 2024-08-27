fe38ebf6eb31c1857d528ea6ca213b2d507dd9a3
robin-li <robin-develop@outlook.com>
2024-08-27 21:23:05 +0800

    reset to hello react

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,13 @@
-import React, {Component} from 'react'
-import {NavLink, Redirect, Route, Switch} from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
+import {useState} from 'react'
 
-export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <div className="row">
-                    <div className="col-xs-offset-2 col-xs-8">
-                        <div className="page-header"><h2>React Router Demo</h2></div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-2 col-xs-offset-2">
-                        <div className="list-group">
+function App() {
+    const [count, setCount] = useState(0)
 
-                            {/* 原生html中，靠<a>跳转不同的页面 */}
-                            {/* <a className="list-group-item" href="./about.html">About</a>
-							<a className="list-group-item active" href="./home.html">Home</a> */}
-
-                            {/* 在React中靠路由链接实现切换组件--编写路由链接 */}
-                            <NavLink className="list-group-item" to="/about">About</NavLink>
-                            <NavLink className="list-group-item" to="/home">Home</NavLink>
-                        </div>
-                    </div>
-                    <div className="col-xs-6">
-                        <div className="panel">
-                            <div className="panel-body">
-                                {/* 注册路由 */}
-                                <Switch>
-                                    <Route path="/about" component={About}/>
-                                    <Route path="/home" component={Home}/>
-									<Redirect to='/home' />
-                                </Switch>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+    return (
+        <>
+            Hello
+        </>
+    )
 }
+
+export default App
