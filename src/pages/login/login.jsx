9ff9dcf5441ca72118da11cf011b35ab119c127e
robin-li <robin-develop@outlook.com>
2024-08-29 21:35:42 +0800

    feat:  获取登录表单数据

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,7 +1,17 @@
 import './login.css'
 import logo from '../../assets/logo.png'
+import {Button, Form, Input, message} from 'antd';
+import {UserOutlined} from '@ant-design/icons';
 
 function Login() {
+
+    const onFinish = (values) => {
+        console.log('Success:', values);
+        message.success('登录成功', 2)
+    };
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
     return <div className="login">
         <header className="login-header">
             <img src={logo} alt="logo"/>
@@ -10,6 +20,49 @@ function Login() {
 
         <section className="login-content">
             <h2>登录</h2>
+
+            <Form
+                name="basic"
+                labelCol={{span: 8}}
+                wrapperCol={{span: 16}}
+                initialValues={{remember: true}}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <Form.Item
+                    prefix={<UserOutlined/>}
+                    label="用户名"
+                    name="username"
+                    rules={
+                    [
+                        {required: true, whitespace:true , message: 'Please input your username!'},
+                        {min: 4, message: '最少4位!'},
+                        {pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是字母数字下划线!'},
+                        {max: 16, message: '最多16位!'},
+                    ]
+                }
+                >
+                    <Input/>
+                </Form.Item>
+                <Form.Item
+                    label="密码"
+                    name="password"
+                    rules={[
+                        {required: true, message: 'Please input your password!'},
+                        {min: 4, message: '密码最少4位'},
+                        {max: 6, message: '密码最多6位'},
+                    ]}
+                >
+                    <Input.Password/>
+                </Form.Item>
+
+                <Form.Item wrapperCol={{offset: 8, span: 16}}>
+                    <Button type="primary" htmlType="submit">
+                        登录
+                    </Button>
+                </Form.Item>
+            </Form>
         </section>
     </div>
         ;
