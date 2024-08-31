bcf847eada0fb42a53921fbfcc32186a8bb03b80
robin-li <robin-develop@outlook.com>
2024-09-01 07:48:27 +0800

    feat: 导航头部

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -1,11 +1,28 @@
 import {Component} from "react";
 import './index.less'
+import logo from '../../assets/logo.png'
+import {Menu} from "antd";
+import {Link} from "react-router-dom";
 
 export default class index extends Component {
+
     render() {
+        const items = [
+            {label: '菜单项一', key: 'item-1'}, // 菜单项务必填写 key
+            {label: '菜单项二', key: 'item-2'},
+            {
+                label: '子菜单',
+                key: 'submenu',
+                children: [{label: '子菜单项', key: 'submenu-item-1'}],
+            },
+        ];
         return (
             <div className='left-nav'>
-                LeftNav
+                <Link to='/' className="left-nav-header">
+                    <img src={logo} alt="logo"/>
+                    <h1>硅谷后台</h1>
+                </Link>
+                <Menu items={items}/>;
             </div>
         )
     }
