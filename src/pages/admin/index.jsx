0e56e77e1350047e3d34e062c1c2ffbe3ad6b9e7
robin-li <robin-develop@outlook.com>
2024-08-31 15:40:16 +0800

    feat: admin layout

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,6 +1,12 @@
 import {Component} from 'react';
+import {Layout} from 'antd';
+
 import memoryUtil from "../../utils/memoryUtil.js";
 import {useNavigate} from "react-router-dom";
+
+import './index.less'
+
+const {Header, Footer, Sider, Content} = Layout;
 
 class Index extends Component {
     render() {
@@ -10,10 +16,14 @@ class Index extends Component {
             navigate('/login');
         }
         return (
-            <div>
-                用户id :{user._id} {<br/>}
-                用户名： {user.username}
-            </div>
+            <Layout style={{minHeight: '100%'}}>
+                <Sider style={{backgroundColor: 'skyblue'}}>Sider</Sider>
+                <Layout>
+                    <Header style={{backgroundColor: 'orange'}}> Header < /Header>
+                    <Content style={{backgroundColor: 'gray'}}>Content</Content>
+                    <Footer style={{textAlign: 'center', color: '#cccccc'}}>推荐使用谷歌浏览器，可以获得更佳页面操作体验</Footer>
+                </Layout>
+            </Layout>
         );
     }
 }
