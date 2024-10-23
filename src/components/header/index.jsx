1b16331efe9de1f0272ff711933e9e04df775967
robin-li <robin-develop@outlook.com>
2024-10-23 22:48:22 +0800

    header 静态页面

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,7 +4,18 @@ class index extends Component {
     render() {
         return (
             <div className='header'>
-                Header
+                <div className='header-top'>
+                    <span>欢迎你，admin</span>
+                    <a href=":javascript">退出</a>
+                </div>
+                <div className='header-bottom'>
+                   <div className='header-bottom-left'>首页</div>
+                   <div className='header-bottom-right'>
+                       <span>2024-10-23 21:49:58</span>
+                       <img alt='weather'></img>
+                       <span>晴</span>
+                   </div>
+                </div>
             </div>
         );
     }
