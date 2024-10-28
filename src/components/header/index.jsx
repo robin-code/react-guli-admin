631fefd1178b8dcec5f5409777d8e012d1c7075f
robin-li <robin-develop@outlook.com>
2024-10-28 22:07:31 +0800

    header时间显示

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,24 +1,38 @@
-import {Component} from 'react';
 import './index.less'
-class index extends Component {
-    render() {
-        return (
-            <div className='header'>
-                <div className='header-top'>
-                    <span>欢迎你，admin</span>
-                    <a href=":javascript">退出</a>
-                </div>
-                <div className='header-bottom'>
-                   <div className='header-bottom-left'>首页</div>
-                   <div className='header-bottom-right'>
-                       <span>2024-10-23 21:49:58</span>
-                       <img alt='weather'></img>
-                       <span>晴</span>
-                   </div>
+import {Button} from "antd";
+import {useEffect, useState} from "react";
+import dayjs from "dayjs";
+
+const Header = () => {
+
+    const [currentTime,setCurrentTime] = useState(dayjs().format('YYYY-MM-DD HH:mm:ss'))
+
+
+    useEffect(() => {
+        // 在组件挂载时设置定时器，每秒更新一次时间
+        const timerId = setInterval(() => {
+            setCurrentTime(dayjs().format('YYYY-MM-DD HH:mm:ss'));
+        }, 1000);
+
+        // 在组件卸载时清除定时器
+        return () => clearInterval(timerId);
+    }, []); // 空依赖数组表示只在组件挂载和卸载时运行
+
+    return (
+        <div className='header'>
+            <div className='header-top'>
+                <span>欢迎你，admin</span>
+                <Button>退出</Button>
+            </div>
+            <div className='header-bottom'>
+                <div className='header-bottom-left'>首页</div>
+                <div className='header-bottom-right'>
+                    <span>{currentTime}</span>
+                    <img alt='weather'></img>
+                    <span>晴</span>
                 </div>
             </div>
-        );
-    }
+        </div>)
 }
 
-export default index;
+export default Header;
