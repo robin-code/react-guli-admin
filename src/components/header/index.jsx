07b65e28f8b67acb91e64b6a883377f8bcecdbf9
robin-li <robin-develop@outlook.com>
2024-10-28 22:21:47 +0800

    根据路径显示标题

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,11 +2,33 @@ import './index.less'
 import {Button} from "antd";
 import {useEffect, useState} from "react";
 import dayjs from "dayjs";
+import {useLocation} from "react-router-dom";
+import menuList from "../../config/menuConfig.js";
+
+
+const getTitleByPath = (menuItems, path) => {
+    for (const item of menuItems) {
+        if (item.key === path) return item.label;
+        if (item.children) {
+            const foundTitle = getTitleByPath(item.children, path);
+            if (foundTitle) return foundTitle;
+        }
+    }
+    return null;
+};
 
 const Header = () => {
 
     const [currentTime,setCurrentTime] = useState(dayjs().format('YYYY-MM-DD HH:mm:ss'))
+    const [title, setTitle] = useState('');
+    const location = useLocation();
 
+
+    useEffect(() => {
+        // 更新路径对应的标题
+        const currentTitle = getTitleByPath(menuList, location.pathname);
+        setTitle(currentTitle || "首页");
+    }, [location.pathname]);
 
     useEffect(() => {
         // 在组件挂载时设置定时器，每秒更新一次时间
@@ -18,6 +40,8 @@ const Header = () => {
         return () => clearInterval(timerId);
     }, []); // 空依赖数组表示只在组件挂载和卸载时运行
 
+
+
     return (
         <div className='header'>
             <div className='header-top'>
@@ -25,7 +49,7 @@ const Header = () => {
                 <Button>退出</Button>
             </div>
             <div className='header-bottom'>
-                <div className='header-bottom-left'>首页</div>
+                <div className='header-bottom-left'>{title}</div>
                 <div className='header-bottom-right'>
                     <span>{currentTime}</span>
                     <img alt='weather'></img>
