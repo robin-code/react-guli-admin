e5a695d068018ffc356e4d2a42ac99af456667ea
robin-li <robin-develop@outlook.com>
2024-11-01 16:05:30 +0800

    环境变量

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,9 @@
 import ajax from './ajax'
 
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
 // 登陆
 /*
 export function reqLogin(username, password) {
   return ajax('/login', {username, password}, 'POST')
 }*/
-export const reqLogin = (username, password) => ajax(BASE_URL + '/api/login', {username, password}, 'POST')
+export const reqLogin = (username, password) => ajax('/api/login', {username, password}, 'POST')
--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,31 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig, loadEnv} from 'vite';
+import react from '@vitejs/plugin-react'; // 如果你使用 React 插件
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8081/', // 目标后端服务器的地址
-                changeOrigin: true, // 是否改变请求源
-                rewrite: (path) => path.replace(/^\/api/, ''), // 可选：重写路径，例如去掉 `/api`
-            },
-        },
-    },
-    css: {
-        preprocessorOptions: {
-            less: {
-                // 在此处配置 Less 选项
-                javascriptEnabled: true,
-                modifyVars: {
-                    '@primary-color': '#1DA57A', // 修改全局变量
+export default defineConfig(({mode}) => {
+    // 加载环境变量
+    const env = loadEnv(mode, './env')
+    return {
+        envDir: 'env',
+        plugins: [react()], // 如果使用 React，添加插件
+        server: {
+            proxy: {
+                '/api': {
+                    target: env.VITE_API_BASE_URL, // 使用环境变量
+                    changeOrigin: true, // 改变请求的来源
+                    rewrite: (path) => path.replace(/^\/api/, ''), // 重写路径
                 },
             },
         },
-    },
-})
+        css: {
+            preprocessorOptions: {
+                less: {
+                    // 在此处配置 Less 选项
+                    javascriptEnabled: true,
+                    modifyVars: {
+                        '@primary-color': '#1DA57A', // 修改全局变量
+                    },
+                },
+            },
+        },
+    };
+});
