5444876b482e4983220cee491d073e80a0a48da6
robin-li <robin-develop@outlook.com>
2024-11-14 22:49:50 +0800

    feat: 展示二级分类优化

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -15,7 +15,7 @@ import {message} from 'antd'
 export default function ajax(url, data = {}, type = 'GET') {
 
     return new Promise((resolve) => {
-        console.info("request ",url,data)
+        console.debug("request ",url,data)
         let promise
         // 1. 执行异步ajax请求
         if (type === 'GET') { // 发GET请求
--- src/pages/category/index.jsx
+++ src/pages/category/index.jsx
@@ -2,46 +2,7 @@ import {Component} from 'react';
 import {Button, Card, message, Space, Table} from "antd";
 import {reqCategory} from "../../api/index.js";
 import LinkButton from "../../components/link-button/index.jsx";
-
-
-const columns = [
-    {
-        title: '编码',
-        dataIndex: 'code',
-        key: 'code',
-    },
-    {
-        title: '分类名称',
-        dataIndex: 'name',
-        key: 'name',
-    },
-    {
-        title: '创建时间',
-        dataIndex: 'createdAt',
-        key: 'createdAt',
-    },
-    {
-        title: '更新时间',
-        dataIndex: 'updatedAt',
-        key: 'updatedAt',
-    },
-    ,
-    {
-        title: '操作',
-        key: 'action',
-        width: 300,
-        render: (category) => (
-            <Space size="middle">
-                <Button onClick={() => this.showSubCategory(category)}>查询子分类</Button>
-                {this.state.parentCode === '0' ?
-                    <Button onClick={() => this.showSubCategorys(category)}>查看子分类</Button> : null}
-                <a>编辑分类</a>
-            </Space>
-        ),
-
-    },
-];
-
+import {ArrowRightOutlined} from "@ant-design/icons";
 
 class Category extends Component {
 
@@ -58,47 +19,28 @@ class Category extends Component {
   初始化Table所有列的数组
    */
     initColumns = () => {
-        this.columns = [
-            {
-                title: '编码',
-                dataIndex: 'code',
-                key: 'code',
-            },
-            {
-                title: '分类名称',
-                dataIndex: 'name',
-                key: 'name',
-            },
-            {
-                title: '创建时间',
-                dataIndex: 'createdAt',
-                key: 'createdAt',
-            },
-            {
-                title: '更新时间',
-                dataIndex: 'updatedAt',
-                key: 'updatedAt',
-            },
-            {
-                title: '操作',
-                key: 'action',
-                width: 300,
-                render: (category) => (
-                    <Space size="middle">
-                        {this.state.parentCode === '0' ?
-                            <Button onClick={() => this.showSubCategorys(category)}>查看子分类</Button> : null}
-                        <a>编辑分类</a>
-                    </Space>
-                ),
+        this.columns = [{
+            title: '编码', dataIndex: 'code', key: 'code',
+        }, {
+            title: '分类名称', dataIndex: 'name', key: 'name',
+        }, {
+            title: '创建时间', dataIndex: 'createdAt', key: 'createdAt',
+        }, {
+            title: '更新时间', dataIndex: 'updatedAt', key: 'updatedAt',
+        }, {
+            title: '操作', key: 'action', width: 300, render: (category) => (<Space size="middle">
+                {this.state.parentCode === '0' ?
+                    <Button onClick={() => this.showSubCategory(category)}>查看子分类</Button> : null}
+                <a>编辑分类</a>
+            </Space>),
 
-            },
-        ]
+        },]
     }
 
     /*
     为第一次render()准备数据
      */
-    componentWillMount() {
+    UNSAFE_componentWillMount() {
         this.initColumns()
     }
 
@@ -112,27 +54,22 @@ class Category extends Component {
     /*
 显示指定一级分类列表
  */
-    showCategorys = () => {
+    showCategory = () => {
         // 更新为显示一列表的状态
         this.setState({
-            parentCode: '0',
-            parentName: '',
-            subCategories: []
+            parentCode: '0', parentName: '', subCategories: []
         })
     }
 
     /*
   显示指定一级分类对象的二子列表
    */
-    showSubCategorys = (category) => {
+    showSubCategory = (category) => {
         // 更新状态
-        console.info("show showSubCategorys,parentCode={}",category.parentCode)
         this.setState({
-            parentCode: category.code,
-            parentName: category.name
-        }, () => { // 在状态更新且重新render()后执行
-            console.log('parentCode=', this.state.code) // '0'
-            // 获取二级分类列表显示
+            parentCode: category.code, parentName: category.name
+        }, () => {
+            // 在状态更新且重新render()后执行
             this.getCategory()
         })
 
@@ -146,23 +83,23 @@ class Category extends Component {
    parentId: 如果没有指定根据状态中的parentId请求, 如果指定了根据指定的请求
     */
     getCategory = async (parentCode) => {
-        console.info("request category list parentCode={}",parentCode)
         // 在发请求前, 显示loading
         this.setState({loading: true})
         parentCode = parentCode || this.state.parentCode
+        console.info("request category list parentCode=%s", parentCode)
         // 发异步ajax请求, 获取数据
         const result = await reqCategory(parentCode)
         // 在请求完成后, 隐藏loading
         this.setState({loading: false})
         if (result.code === 0) {
             const categories = result.data
-            console.info("category ={}", categories)
+            console.debug("category ={}", categories)
             if (parentCode === '0') {
                 // 更新一级分类状态
                 this.setState({
                     categories
                 })
-                console.log('----', this.state.categories.length)
+                console.debug('----', this.state.categories.length)
             } else {
                 // 更新二级分类状态
                 this.setState({
@@ -176,30 +113,25 @@ class Category extends Component {
 
     render() {
         const {categories, subCategories, parentCode, parentName, loading, showStatus} = this.state
-        const category = this.category || {} // 如果还没有指定一个空对象
-        // card的左侧
-        const title = parentCode === '0' ? '一级分类列表' : (
-            <span>
-        <LinkButton onClick={this.showCategorys}>一级分类列表</LinkButton>
+        const title = parentCode === '0' ? '一级分类列表' : (<span>
+        <LinkButton onClick={this.showCategory}>一级分类列表</LinkButton>
+       <ArrowRightOutlined style={{marginRight: 10}}/>
         <span>{parentName}</span>
-      </span>
-        )
+      </span>)
         const extra = <Button type='primary'>
             添加
         </Button>;
-        return (
-            <Card
-                title={title}
-                extra={extra}
-            >
-                <Table
-                    loading={loading}
-                    rowKey='id'
-                    bordered dataSource={parentCode === '0' ? categories : subCategories} columns={this.columns}
-                    pagination={{defaultPageSize: 5, showQuickJumper: true}}
-                />
-            </Card>
-        );
+        return (<Card
+            title={title}
+            extra={extra}
+        >
+            <Table
+                loading={loading}
+                rowKey='id'
+                bordered dataSource={parentCode === '0' ? categories : subCategories} columns={this.columns}
+                pagination={{defaultPageSize: 5, showQuickJumper: true}}
+            />
+        </Card>);
     }
 
 
